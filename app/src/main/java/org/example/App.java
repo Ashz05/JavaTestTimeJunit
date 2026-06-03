d53b91b42afa58f149e853d7adc768b40eb787ee
Ashwin <ashwinkp756@gmail.com>
2026-06-03 15:10:04 +0530

    Test BubbleSort

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -8,5 +8,17 @@ public class App{
         }
         return arr;
     }
+    public int[] BubbleSort(int... arr){
+        for(int i = 0 ; i < arr.length ; i++){
+            for(int j = 0 ; j < arr.length -1 ; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
 
 }
--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertTimeout;
 public class AppTest{
     App sorting;
@@ -18,6 +19,15 @@ public class AppTest{
     @Test void TestApp(){
         assertTimeout(Duration.ofMillis(2), () -> sorting.TimeCompilation(4,3,12,2,1,9,6,5,8));
         System.out.println("Actual Test Running...!");
+    }
+
+    @Test void TestBubbleSort(){
+        int[] result = sorting.BubbleSort(5,4,2,3,1,10,9,6,7,8);
+        int[] expected = {1,2,3,4,5,6,7,8,9,10};
+
+        assertArrayEquals(expected,result);
+        assertTimeout(Duration.ofNanos(1), () ->
+                sorting.BubbleSort(5,4,2,1000,124,53,3,1,10,9,6,7,8,100,12,23,42,41,34,123,0));
     }
 
     @AfterEach
